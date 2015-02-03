d748c618897edb5ae5e5e07ccec80f55cf87f31b
Dane Hillard <github@danehillard.com>
2015-02-02 23:41:48 -0500

    Updates to album and song display/admin

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Album(models.Model):
     title = models.CharField(max_length=255)
-    code = models.SlugField(max_length=20)
+    code = models.SlugField(max_length=50)
     synopsis = models.TextField()
     cover_art_height = models.IntegerField()
     cover_art_width = models.IntegerField()
@@ -15,9 +15,12 @@ class Album(models.Model):
 
 class Song(models.Model):
     title = models.CharField(max_length=255)
-    code = models.SlugField(max_length=20)
+    code = models.SlugField(max_length=50)
     lyrics = models.TextField()
     album = models.ForeignKey(Album)
+    art_height = models.IntegerField()
+    art_width = models.IntegerField()
+    art = models.ImageField(upload_to='song_art', height_field='art_height', width_field='art_width')
 
     def __str__(self):
         return self.title
