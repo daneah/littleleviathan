95e30795ccf016ba69c91ea028f1632c0a9258ae
Dane Hillard <github@danehillard.com>
2015-02-03 12:14:08 -0500

    Add test for album_list view

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from main.models import Album
 
 
 class HomePageTestCase(TestCase):
@@ -7,3 +10,8 @@ class HomePageTestCase(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main/index.html')
+
+    def test_album_list_view(self):
+        response = self.client.get('/albums/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/album_list.html')
