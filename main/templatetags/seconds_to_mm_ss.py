f61c6f8857fc13c72e5b2874a98c498d029617be
Dane Hillard <github@danehillard.com>
2015-02-04 09:07:47 -0500

    Turn song duration seconds into mm:ss format

--- main/templatetags/seconds_to_mm_ss.py
+++ main/templatetags/seconds_to_mm_ss.py
@@ -7,5 +7,5 @@ register = template.Library()
 @register.filter
 def seconds_to_mm_ss(seconds):
     minutes = floor(seconds / 60)
-    seconds = floor(minutes - (minutes * 60))
+    seconds = floor(seconds - (minutes * 60))
     return '%d:%02d' % (minutes, seconds)
