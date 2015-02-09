89279e8d283aa9a495de3b50fca7a7bf27c7c5da
Dane Hillard <github@danehillard.com>
2015-02-09 11:51:32 -0500

    Add error pages

--- littleleviathan/urls.py
+++ littleleviathan/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -16,5 +18,11 @@ urlpatterns = patterns(
     url(r'^', include('main.urls', namespace='main')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        url(r'^404/$', TemplateView.as_view(template_name='404.html'), name='not_found'),
+        url(r'^500/$', TemplateView.as_view(template_name='500.html'), name='server_error')
+    )
 
 urlpatterns += staticfiles_urlpatterns()
