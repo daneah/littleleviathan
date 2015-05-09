e206917b5fe6db8292832413bce7e474860e87bf
Dane Hillard <github@danehillard.com>
2015-05-09 16:56:05 -0400

    Add robots.txt

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from main.views import HomeView, AlbumListView, AlbumDetailView, SongDetailView
 
@@ -8,4 +9,5 @@ urlpatterns = patterns(
     url(r'^albums/$', AlbumListView.as_view(), name='albums'),
     url(r'^album/(?P<album_code>[\w\-]+)/$', AlbumDetailView.as_view(), name='album'),
     url(r'^album/(?P<album_code>[\w\-]+)/song/(?P<song_code>[\w\-]+)/$', SongDetailView.as_view(), name='song'),
+    url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 )
