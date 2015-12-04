fe3891dd9e621815d49599f3be963e17b9794c9a
Dane Hillard <github@danehillard.com>
2015-12-03 22:59:22 -0500

    Upgrade to Django 1.9

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
 from main.views import HomeView, AlbumListView, AlbumDetailView, SongDetailView
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^albums/$', AlbumListView.as_view(), name='albums'),
-    url(r'^album/(?P<album_code>[\w\-]+)/$', AlbumDetailView.as_view(), name='album'),
-    url(r'^album/(?P<album_code>[\w\-]+)/song/(?P<song_code>[\w\-]+)/$', SongDetailView.as_view(), name='song'),
-    url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-)
+    url(r'^album/(?P<album_code>[\w\-]+)/song/(?P<song_code>[\w\-]+)/', SongDetailView.as_view(), name='song'),
+    url(r'^album/(?P<album_code>[\w\-]+)/', AlbumDetailView.as_view(), name='album'),
+    url(r'^albums/', AlbumListView.as_view(), name='albums'),
+    url(r'^robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+]
