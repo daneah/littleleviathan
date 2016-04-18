e4b894beea40e6462483f2bbca657203f0148452
Dane Hillard <github@danehillard.com>
2016-04-18 08:57:26 -0400

    Move to configuration module

---
+++ configuration/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.conf import settings
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from sitemaps import AlbumSitemap, SongSitemap
+
+sitemaps = {
+    'albums': AlbumSitemap,
+    'songs': SongSitemap,
+}
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('main.urls', namespace='main')),
+    url(r'^sitemap\.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^400/', TemplateView.as_view(template_name='400.html'), name='bad_request'),
+        url(r'^403/', TemplateView.as_view(template_name='403.html'), name='permission_denied'),
+        url(r'^404/', TemplateView.as_view(template_name='404.html'), name='not_found'),
+        url(r'^500/', TemplateView.as_view(template_name='500.html'), name='server_error'),
+    ]
+
+urlpatterns += staticfiles_urlpatterns()
