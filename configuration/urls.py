ef5a551ed19e84723e5151a1e736cc5986f25a40
Dane Hillard <github@danehillard.com>
2016-04-22 17:53:23 -0400

    Relayout album detail page

--- configuration/urls.py
+++ configuration/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
-from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from sitemaps import AlbumSitemap, SongSitemap
@@ -24,6 +25,6 @@ if settings.DEBUG:
         url(r'^403/', TemplateView.as_view(template_name='403.html'), name='permission_denied'),
         url(r'^404/', TemplateView.as_view(template_name='404.html'), name='not_found'),
         url(r'^500/', TemplateView.as_view(template_name='500.html'), name='server_error'),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
