93cd2857acaa4ffcb09f516939496adaccc75860
Dane Hillard <github@danehillard.com>
2019-01-24 10:27:49 -0500

    Add 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,58 @@
+<template>
+  <main>
+    <h1>Lost at sea!</h1>
+
+    <p>
+      The page you requested could not be found.
+    </p>
+    <router-link
+      :to="{ name: 'home' }"
+    >
+      Go back home
+    </router-link>
+  </main>
+</template>
+
+<script>
+const PAGE_TITLE = 'Page Not Found';
+const PAGE_DESCRIPTION = 'This page does not exist';
+
+export default {
+  name: 'NotFound',
+  metaInfo: {
+    title: 'Page Not Found',
+    meta: [
+      {
+        name: 'description',
+        content: PAGE_DESCRIPTION,
+        vmid: 'description',
+      },
+      {
+        property: 'og:description',
+        content: PAGE_DESCRIPTION,
+        vmid: 'og:description',
+      },
+      {
+        property: 'og:title',
+        content: `${PAGE_TITLE} | Little Leviathan`,
+        vmid: 'og:title',
+      },
+      {
+        property: 'og:url',
+        content: window.location.href,
+        vmid: 'og:url',
+      },
+    ],
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  text-align: center;
+}
+
+p {
+  margin: 1rem auto;
+}
+</style>
