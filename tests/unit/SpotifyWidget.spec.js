3b361f010b23e965620f0c18951724397f5b718f
Dane Hillard <github@danehillard.com>
2019-01-24 10:07:16 -0500

    Add Travis CI config

---
+++ tests/unit/SpotifyWidget.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils';
+
+import SpotifyWidget from '@/components/SpotifyWidget.vue';
+
+
+describe('SpotifyWidget', () => {
+  it('Renders an iframe with the correct src for Spotify', () => {
+    const wrapper = shallowMount(SpotifyWidget, {
+      propsData: {
+        artistId: 'Little Leviathan',
+      },
+    });
+
+    const iframe = wrapper.find('iframe');
+    expect(iframe.attributes()['src']).toBe('https://open.spotify.com/follow/1/?uri=spotify:artist:Little Leviathan&size=basic&theme=light&show-count=0');
+  });
+})
