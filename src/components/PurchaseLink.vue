73e433766dacb21cddb9f008d21e93e5391dbd6b
Dane Hillard <github@danehillard.com>
2019-01-26 15:58:47 -0500

    Create PurchaseLink component

---
+++ src/components/PurchaseLink.vue
@@ -0,0 +1,45 @@
+<template>
+  <a
+    :href="href"
+    :title="`Listen on ${service}`"
+  >
+    <FontAwesome
+      :icon="icon"
+      :alt="service"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'PurchaseLink',
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+    service: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: [String, Object],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+a {
+  padding: .75rem;
+
+  svg {
+    transition: transform 0.25s cubic-bezier(0.68, -0.75, 0.265, 1.75);
+  }
+
+  &:hover {
+    transform: scale(1.2);
+  }
+}
+</style>
