e9acba0059de682c05d0c21b3e12e3b4b13dfa51
Dane Hillard <github@danehillard.com>
2019-01-26 15:04:13 -0500

    Use FontAwesome icons for purchase links (#18)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import Vue from 'vue';
 import VueAnalytics from 'vue-analytics';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMusic } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBandcamp,
+  faSoundcloud,
+  faSpotify,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
-Vue.config.productionTip = false;
+// FontAwesome
+library.add(faBandcamp, faMusic, faSoundcloud, faSpotify);
+Vue.component('FontAwesome', FontAwesomeIcon);
 
+// Google Analytics
 Vue.use(VueAnalytics, {
   id: 'UA-47967013-1',
   router,
@@ -15,6 +27,9 @@ Vue.use(VueAnalytics, {
     sendHitTask: process.env.NODE_ENV === 'production',
   },
 });
+
+// Main setup
+Vue.config.productionTip = false;
 
 new Vue({
   router,
